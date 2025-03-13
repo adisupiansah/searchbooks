f2f7057c408dc36f0d45c748c25e54192490e7c5
adisupiansah <adisupiansah15@gmail.com>
2025-03-13 22:21:43 +0700

    Update 13-3-2025

--- src/components/AuthLogin.jsx
+++ src/components/AuthLogin.jsx
@@ -1,29 +1,45 @@
 "use client";
-import React from 'react'
-import { signIn } from "next-auth/react"
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
-import Image from 'next/image';
-import logo from '/public/img/logo.ico'
+import Image from "next/image";
+import logo from "/public/img/logo.ico";
+import app from "@/firebase/Firebase";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const AuthLogin = () => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      result.user;
+      window.location.href = "/";
+    } catch (error) {
+      console.error("terdapat error:", error);
+    }
+  };
+
   return (
-    <div className='authlogin'>
+    <div className="authlogin">
       <div className="container">
         <div className="row flex justify-center items-center vh-100">
           <div className="col-md-6">
             <div className="card border-0">
               <div className="card-body">
                 <div className="logo flex flex-column justify-center items-center mb-5">
-                  <Image src={logo} width={110} height={110} alt='logo'/>
-                  <h5 className="card-title text-center mt-2">Sign In To SearcBooks</h5>
+                  <Image src={logo} width={110} height={110} alt="logo" />
+                  <h5 className="card-title text-center mt-2">
+                    Sign In To SearcBooks
+                  </h5>
                 </div>
                 <div className="login-google flex justify-center items-center bg-gray-400 col-md-3 rounded-md mx-auto border border-gray-800 hover:bg-gray-500  ">
-                  <button 
-                  className='text-black p-1 flex justify-center items-center gap-2'
-                  onClick={() => signIn("google", {callbackUrl: '/', prompt: 'select_account'})}
+                  <button
+                    className="text-black p-1 flex justify-center items-center gap-2"
+                    onClick={handleGoogleLogin}
                   >
                     <span>login</span>
-                    <FcGoogle/>
+                    <FcGoogle />
                   </button>
                 </div>
               </div>
@@ -32,7 +48,7 @@ const AuthLogin = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthLogin
+export default AuthLogin;
--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,11 +1,20 @@
 import { initializeApp } from "firebase/app";
-import {getDatabase, ref, set, get, remove} from "firebase/database";
+import { getDatabase, ref, set, get, remove } from "firebase/database";
 
 const firebaseConfig = {
-    databaseURL: "https://booksapi-890ba-default-rtdb.firebaseio.com/",
+  databaseURL: "https://booksapi-890ba-default-rtdb.firebaseio.com/",
+  apiKey: "AIzaSyADwbkb1l-79hOOpQfedKoMq59h2qAcMUU",
+  authDomain: "booksapi-890ba.firebaseapp.com",
+  databaseURL: "https://booksapi-890ba-default-rtdb.firebaseio.com",
+  projectId: "booksapi-890ba",
+  storageBucket: "booksapi-890ba.firebasestorage.app",
+  messagingSenderId: "778541962257",
+  appId: "1:778541962257:web:ba4997d67c206f1b330db0",
+  measurementId: "G-MENHE884YV",
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-export { database, ref, set, get, remove }
+export { database, ref, set, get, remove };
+export default app;
